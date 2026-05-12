8e713aef27eb20e76ad3aad07ccc8ffe44c4a9db
fy-codes <fth3347yldz@gmail.com>
2026-05-12 22:09:11 +0300

    add task delete feature

--- js/app.js
+++ js/app.js
@@ -13,11 +13,22 @@ taskForm.addEventListener("submit", function(event) {
     }
 
     const li = document.createElement("li");
-    li.className = "list-group-item";
-    li.textContent = taskText;
+
+    li.className = "list-group-item d-flex justify-content-between align-items-center";
+    li.innerHTML = `
+        <span>${taskText}</span>
+        <button class = "btn btn-danger btn-sm delete-btn">Delete</button>
+    `;
 
     taskList.appendChild(li);
 
     taskInput.value = "";
     
+});
+
+taskList.addEventListener("click", function(event){
+    
+    if(event.target.classList.contains("delete-btn")){
+        event.target.parentElement.remove();
+    }
 });
