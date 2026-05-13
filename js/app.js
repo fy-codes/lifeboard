5061864e003c5efa9d39f2a9e982d7aa1e6f435e
fy-codes <fth3347yldz@gmail.com>
2026-05-13 19:40:19 +0300

    add localStorage support for tsaks

--- js/app.js
+++ js/app.js
@@ -2,25 +2,44 @@ const taskForm = document.querySelector("#task-form");
 const taskInput = document.querySelector("#task-input");
 const taskList = document.querySelector("#task-list");
 
+let tasks = [];
+
+function createTaskElement(task){
+
+    const li = document.createElement("li");
+    li.className = "list-group-item d-flex justify-content-between align-items-center";
+    li.innerHTML = `
+        <span>${task.text}</span>
+
+        <button class = "btn btn-danger btn-sm delete-btn">
+            Delete
+        </button>
+    `;
+
+    taskList.appendChild(li);
+}
 
 taskForm.addEventListener("submit", function(event) {
 
     event.preventDefault();
 
     const taskText = taskInput.value;
+
     if(taskText.trim() === ""){
         return;
     }
 
-    const li = document.createElement("li");
+    const task = {
+        id: Date.now(),
+        text: taskText,
+        completed: false
+    };
 
-    li.className = "list-group-item d-flex justify-content-between align-items-center";
-    li.innerHTML = `
-        <span>${taskText}</span>
-        <button class = "btn btn-danger btn-sm delete-btn">Delete</button>
-    `;
+    tasks.push(task);
 
-    taskList.appendChild(li);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    createTaskElement(task);
 
     taskInput.value = "";
     
@@ -31,4 +50,18 @@ taskList.addEventListener("click", function(event){
     if(event.target.classList.contains("delete-btn")){
         event.target.parentElement.remove();
     }
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+    if(storedTasks !== null){
+        tasks = storedTasks;
+    }
+
+    tasks.forEach(function(task){
+        createTaskElement(task);
+    });
+
 });
